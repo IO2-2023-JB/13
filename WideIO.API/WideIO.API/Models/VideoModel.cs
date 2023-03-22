abd72077d30eaa220267b5e599f0286a6f71aabb
Michał Czapnik <01161283@pw.edu.pl>
2023-03-22 21:04:40 +0000

    Added 5 files to /WideIO.API/WideIO.API/Models

---
+++ WideIO.API/WideIO.API/Models/VideoModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+namespace MyVideIO.Models
+{
+    public class VideoModel
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        [Required(ErrorMessage = "Please enter the title of your video")]
+        public string Title { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        public int Duration { get; set; } //in ms?
+
+        public int PositiveReactions { get; set; }
+
+        public int NegativeReactions { get; set; }
+
+        public IEnumerable<CommentModel> Comments { get; set; }
+        
+
+    }
+}

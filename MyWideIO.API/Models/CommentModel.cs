e2829abbc30eb21a22606a3dafe5bedfd7af839e
Hubert Lawenda <01161387@pw.edu.pl>
2023-03-22 23:22:33 +0100

    Poprawka znacznikow konfliktow

--- MyWideIO.API/Models/CommentModel.cs
+++ MyWideIO.API/Models/CommentModel.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:MyWideIO.API/MyWideIO.API/Models/CommentModel.cs
 ﻿using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -24,31 +23,3 @@ namespace MyVideIO.Models
 
     }
 }
-=======
-﻿using Microsoft.AspNetCore.Identity;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Runtime.CompilerServices;
-
-namespace MyVideIO.Models
-{
-    public class CommentModel 
-    {
-
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
-
-        [Required]
-        [StringLength(500)]
-        public string Content { get; set; }
-
-        [Required]
-        public ViewerModel Author { get; set; }
-
-        public IEnumerable<CommentModel> Replies { get; set;}
-
-    }
-}
->>>>>>> origin/develop:WideIO.API/WideIO.API/Models/CommentModel.cs

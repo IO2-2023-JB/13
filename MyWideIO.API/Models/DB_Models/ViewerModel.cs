c56445e8f779f5fb95eb076ca8d73f1dad1df8ad
Maciej Podgórski <01151542@pw.edu.pl>
2023-04-01 05:41:26 +0200

    poprawki male

--- MyWideIO.API/Models/DB_Models/ViewerModel.cs
+++ MyWideIO.API/Models/DB_Models/ViewerModel.cs
@@ -10,6 +10,10 @@ namespace MyWideIO.API.Models.DB_Models
     public class ViewerModel : IdentityUser<Guid>
     {
 
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Surname { get; set; } = string.Empty;
         public string Nick { get; set; } = string.Empty;
 
         public ICollection<ViewerWatchLater> WatchLater { get; set; }

25e2e865acd9a235b52d43bf32097c5e208b20db
Adioos5 <adrian.ciesla.stud@pw.edu.pl>
2023-04-05 13:03:16 +0200

    Added sketch of administrators page

--- Frontend/src/User_Account/Register.js
+++ Frontend/src/User_Account/Register.js
@@ -135,7 +135,7 @@ const Register = () => {
                 </p>
             </section>
         ): (
-        <section>
+        <section class="container-fluid justify-content-center" style={{marginTop:"200px"}}>
             <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
             <h1>Register</h1>
             <form onSubmit={handleSubmit}>

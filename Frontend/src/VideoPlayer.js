baecf194bc3f8421009d83c050341e5fe8415799
wolny33 <patrykburzycki@op.pl>
2023-04-28 19:16:45 +0200

    thumbnails at video list at profile page added

--- Frontend/src/VideoPlayer.js
+++ Frontend/src/VideoPlayer.js
@@ -9,6 +9,7 @@ import 'video-react/dist/video-react.css';
 import {faCheck, faTimes, faInfoCircle  } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import ReactPlayer from 'react-player';
 
 const VIDEO_URL = '/video';
 const METADATA_URL = '/video-metadata';
@@ -71,7 +72,7 @@ const VideoPlayer = () => {
   });
 
   useEffect(() => {
-    console.log("Updated reactionsData:", reactionsData);
+    //console.log("Updated reactionsData:", reactionsData);
     setIsLoading(false);
   }, [reactionsData]);
 
@@ -86,23 +87,20 @@ const VideoPlayer = () => {
     localStorage.setItem("lastVisitedPage", location.pathname);
   })
 
-  useEffect(() => {
+  useEffect( () => {
     if(params.videoid)
     {
       video_id = params.videoid;
-      videoUrl = baseURL + VIDEO_URL + "/" + video_id + "?access_token=" + auth.accessToken;
+      videoUrl = baseURL + VIDEO_URL + "/" + video_id + "?access_token=" + auth?.accessToken;
     }
     else
     {
-      video_id = "7373CB25-946C-460A-F7C8-08DB425CABE3";
-      videoUrl = baseURL + VIDEO_URL + "/" + video_id + "?access_token=" + auth.accessToken;
-      const videoElement = videoRef.current;
-      videoElement.src = videoUrl;
-      videoElement.load();
+      navigate('/home');
     }//to delete
     if(video_id){
-      setIsLoading(true);
       //get video metadata:
+      if(auth.accessToken){
+      setIsLoading(true);
       axios.get(METADATA_URL + "?id=" + video_id,
           {
             headers: { 
@@ -124,6 +122,7 @@ const VideoPlayer = () => {
             setErrMsg('Forbidden');
         } else if(err.response?.status === 403){
           setErrMsg('Not found');
+          //navigate('/home');
         } else {
             setErrMsg('Getting metadata failed');
         }
@@ -138,11 +137,13 @@ const VideoPlayer = () => {
             withCredentials: true
           }
       ).then(response => {
+        console.log(response.data);
         setReactionsData({
           positiveCount: response.data.positiveCount,
           negativeCount: response.data.negativeCount,
           currentUserReaction: response.data.currentUserReaction,
         });
+        setIsLoading(false);
       }).catch(err => {
         if(!err?.response) {
             setErrMsg('No Server Response')
@@ -154,8 +155,9 @@ const VideoPlayer = () => {
             setErrMsg('Getting metadata failed');
         }
       });
+      }
     }
-  }, [params.videoid])
+  }, [params.videoid, auth?.accessToken, auth?.id])
 
   const handleEditClick = () => {
     setEditMode(true);
@@ -307,38 +309,25 @@ const VideoPlayer = () => {
             withCredentials: true //cred
         }
       );
+      let positive = reactionsData.positiveCount;
+      let negative = reactionsData.negativeCount;
+      if(reactionsData.currentUserReaction == 'Positive' && reaction != 'Positive'){
+        positive--;
+      }
+      if(reactionsData.currentUserReaction == 'Negative' && reaction != 'Negative'){
+        negative--;
+      }
+      if(reaction == 'Positive' && reactionsData.currentUserReaction != 'Positive'){
+        positive++;
+      }
+      if(reaction == 'Negative' && reactionsData.currentUserReaction != 'Negative'){
+        negative++;
+      }
       setReactionsData({
-        positiveCount: reactionsData.positiveCount,
-        negativeCount: reactionsData.negativeCount,
+        positiveCount: positive,
+        negativeCount: negative,
         currentUserReaction: reaction,
       });
-
-      await axios.get(REACTION_URL + "?id=" + video_id,
-          {
-            headers: { 
-              'Content-Type': 'application/json',
-              "Authorization" : `Bearer ${auth?.accessToken}`
-            },
-            withCredentials: true
-          }
-      ).then(response => {
-        setReactionsData({
-          positiveCount: response.data.positiveCount,
-          negativeCount: response.data.negativeCount,
-          currentUserReaction: response.data.currentUserReaction,
-        });
-      }).catch(err => {
-        if(!err?.response) {
-            setErrMsg('No Server Response')
-        } else if(err.response?.status === 400) {
-            setErrMsg('Bad request');
-        } else if(err.response?.status === 404){
-          setErrMsg('Not found');
-        } else {
-            setErrMsg('Getting metadata failed');
-        }
-      });
-
     } catch(err){
       if (!err?.response) {
         setErrMsg('No Server Response');
@@ -380,9 +369,18 @@ const VideoPlayer = () => {
       justifyContent: "flex-start", marginTop: "150px", width: "900px", backgroundColor:"#333333", borderTopRightRadius: "25px", borderTopLeftRadius: "25px"}}>
       <div class="container-fluid justify-content-center" style={{marginTop: "50px", width: "900px",}}>
 
-        <video id="videoPlayer" width="830" controls ref={videoRef}>
+        {/* <video id="videoPlayer" width="830" controls autoplay loop muted playsinline ref={videoRef}>
           <source src={videoUrl} type="video/mp4" />
-        </video>
+        </video> */}
+        <ReactPlayer
+          id="videoPlayer"
+          ref={videoRef}
+          url={videoUrl}
+          playing={true}
+          width={830}
+          controls={true}
+          loop={true}
+        />
         
       </div>
       {!editMode?(
@@ -397,14 +395,14 @@ const VideoPlayer = () => {
           </div>
           <div class="container-fluid justify-content-center" style={{marginTop:"20px", borderRadius:"15px", paddingBottom:"20px", paddingTop:"0px", backgroundColor:"#282828"}}>
             {reactionsData.currentUserReaction == 'Positive'?(
-              <button class="btn btn-light" style={{marginRight:"20px"}} onClick={reactNone}>
+              <button class="btn btn-light" style={{marginRight:"20px"}} onClick={reactNone} ref={reactionsData}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
                   <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"/>
                 </svg>
                 {' ' + `${reactionsData.positiveCount}`}
               </button>
             ):(
-              <button class="btn btn-dark" style={{marginRight:"20px"}} onClick={reactPositive}>
+              <button class="btn btn-dark" style={{marginRight:"20px"}} onClick={reactPositive} ref={reactionsData}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
                   <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"/>
                 </svg>
@@ -530,7 +528,7 @@ const VideoPlayer = () => {
                             //style={{ display: 'none' }}
                             accept="image/*"
                             id="thumbnail_picture"
-                            // onChange={handle_picture}
+                            onChange={handle_picture}
                             defaultValue={thumbnail_picture_name}
                             aria-describedby="confirmnote"
                             onFocus={() => setThumbnail_pictureFocus(true)}

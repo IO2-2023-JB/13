76e0f3093aeae6e113d764b8eea7eeae827b742a
Adioos5 <adrian.ciesla.stud@pw.edu.pl>
2023-05-26 06:48:00 +0200

    Added videos searching logic

--- Frontend/src/Search.js
+++ Frontend/src/Search.js
@@ -1,23 +1,54 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from './api/axios';
 
-function Search(props) {
+const SEARCH_URL = '/search';
 
-    const [videosData, setVideosData] = useState(null);
+const Search = (props) => {
+
+    const [videosData, setVideosData] = useState([]);
 
     useEffect(() => {
-      axios.get(SEARCH_URL + "?query="+ inputValue + "&sortingCriterion=1&sortingType=1", {
+      axios.get(SEARCH_URL, {
+        params:{
+          query: props.query,
+          sortingCriterion: 1,
+          sortingType: 1
+        },
         headers: { 
           'Content-Type': 'application/json',
-        },
-        withCredentials: true 
+        }
       })
       .then(response => {
-        setVideosData(response?.data);
+        setVideosData(response?.data?.videos);
       })
       .catch(error => {
         console.log("error: ", error);
       });
     });
+
   // Component logic
-  return <div>Hello, {props.query}!</div>;
+  return (
+    <div style={{marginTop:"200px"}} class="container">
+      Hello, {props.query}!
+      <ul style={{padding:"0px", display:"inline"}}>
+          {videosData.map(video => (
+              <li class="mx-5" style={{marginTop:"20px", color:"white", borderRadius:"15px",
+                      padding:"20px", backgroundColor:"#222222"}}>
+                  <div class="row">
+                      <div class="col-sm">
+                          {video.title}
+                      </div>
+                      <div class="col-sm">
+                          <h4>Nickname:</h4>
+                          <h1 style={{marginTop:"30px"}}>{video.authorNickname}</h1>
+                      </div>
+                  </div>
+              </li>
+          )).reverse()}
+      </ul>
+    </div>
+  );
 }
+
+export default Search;

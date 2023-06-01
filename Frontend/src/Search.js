9cbe4d12a7ccde2c011f14b48a98db3e9ba7c24e
Adioos5 <adrian.ciesla.stud@pw.edu.pl>
2023-06-01 13:00:42 +0200

    Separated search results into 3 columns

--- Frontend/src/Search.js
+++ Frontend/src/Search.js
@@ -40,91 +40,97 @@ const Search = (props) => {
   // Component logic
   return (
     <div style={{marginTop:"200px"}} class="container">
+          <div class="row" style={{marginBottom:"100px"}}>
+            <div class="col-sm">
+              <h2 class="display-5"> Videos: </h2>
+              <ul style={{padding:"0px", display:"inline"}}>
+                  {videosData.map(video => (
+                      <li className="search_list_item" class="mx-5" style={{marginTop:"20px", color:"white", borderRadius:"15px",
+                              padding:"20px", backgroundColor:"#222222", cursor: "pointer"}} onClick={() => handleVideoClick(video.id)}>
+                          <div class="row">
+                            <div className="box3" style={{width:"200px", height:"170px", cursor: "pointer"}}>
+                              <div className="box4" style={{width:"180px", height:"150px", backgroundImage: `url(${video.thumbnail})`, backgroundRepeat:"no-repeat", backgroundSize:"cover", backgroundPosition:"center"}}>
+                                  <table style={{backgroundColor: "transparent"}}>
+                                      <tr style={{backgroundColor: "transparent"}}>
+                                      <div className="movie_thumbnail" style={{width:"280px", height:"60px", backgroundSize:"cover", backgroundColor: "transparent" } } onClick={() => handleVideoClick(video.id)}>
 
-          <h2 class="display-5"> Videos: </h2>
-          <ul style={{padding:"0px", display:"inline"}}>
-              {videosData.map(video => (
-                  <li className="search_list_item" class="mx-5" style={{marginTop:"20px", color:"white", borderRadius:"15px",
-                          padding:"20px", backgroundColor:"#222222", cursor: "pointer"}} onClick={() => handleVideoClick(video.id)}>
-                      <div class="row">
-                        <div className="box3" style={{width:"300px", height:"170px", cursor: "pointer"}}>
-                          <div className="box4" style={{width:"280px", height:"150px", backgroundImage: `url(${video.thumbnail})`, backgroundRepeat:"no-repeat", backgroundSize:"cover", backgroundPosition:"center"}}>
-                              <table style={{backgroundColor: "transparent"}}>
-                                  <tr style={{backgroundColor: "transparent"}}>
-                                  <div className="movie_thumbnail" style={{width:"280px", height:"60px", backgroundSize:"cover", backgroundColor: "transparent" } } onClick={() => handleVideoClick(video.id)}>
+                                      </div>
+                                      </tr>
+                                  </table>
+                                </div> 
+                            </div>
+                            <div class="col-sm">
+                                <h1 style={{marginTop:"30px"}}>{video.title}</h1>
+                                <h4 style={{marginTop:"30px"}}>{video.authorNickname}</h4>
+                            </div>
+                            <div class="col-sm">
+                                <h4 style={{marginTop:"30px"}}>Views: {video.viewCount}</h4>
+                            </div>
+                          </div>
+                      </li>
+                  )).reverse()}
+              </ul>
+            </div>
+            <div class="col-sm">
+              <h2 class="display-5"> Users: </h2>
+              <ul style={{padding:"0px", display:"inline"}}>
+                  {usersData.map(user => (
+                      <li className="search_list_item" class="mx-5" style={{marginTop:"20px", color:"white", borderRadius:"15px",
+                              padding:"20px", backgroundColor:"#222222", cursor: "pointer"}} >
+                          <div class="row">
+                            <div className="box3" style={{width:"200px", height:"170px", cursor: "pointer"}}>
+                              <div className="box4" style={{width:"180px", height:"150px", backgroundImage: `url(${user.avatarImage})`, backgroundRepeat:"no-repeat", backgroundSize:"cover", backgroundPosition:"center"}}>
+                                  <table style={{backgroundColor: "transparent"}}>
+                                      <tr style={{backgroundColor: "transparent"}}>
+                                      <div className="movie_thumbnail" style={{width:"280px", height:"60px", backgroundSize:"cover", backgroundColor: "transparent" } }>
 
-                                  </div>
-                                  </tr>
-                              </table>
-                            </div> 
-                        </div>
-                        <div class="col-sm">
-                            <h1 style={{marginTop:"30px"}}>{video.title}</h1>
-                            <h4 style={{marginTop:"30px"}}>{video.authorNickname}</h4>
-                        </div>
-                        <div class="col-sm">
-                            <h4 style={{marginTop:"30px"}}>Views: {video.viewCount}</h4>
-                        </div>
-                      </div>
-                  </li>
-              )).reverse()}
-          </ul>
-            <h2 class="display-5"> Users: </h2>
-          <ul style={{padding:"0px", display:"inline"}}>
-              {usersData.map(user => (
-                  <li className="search_list_item" class="mx-5" style={{marginTop:"20px", color:"white", borderRadius:"15px",
-                          padding:"20px", backgroundColor:"#222222", cursor: "pointer"}} >
-                      <div class="row">
-                        <div className="box3" style={{width:"300px", height:"170px", cursor: "pointer"}}>
-                          <div className="box4" style={{width:"280px", height:"150px", backgroundImage: `url(${user.avatarImage})`, backgroundRepeat:"no-repeat", backgroundSize:"cover", backgroundPosition:"center"}}>
-                              <table style={{backgroundColor: "transparent"}}>
-                                  <tr style={{backgroundColor: "transparent"}}>
-                                  <div className="movie_thumbnail" style={{width:"280px", height:"60px", backgroundSize:"cover", backgroundColor: "transparent" } }>
+                                      </div>
+                                      </tr>
+                                  </table>
+                                </div> 
+                            </div>
+                            <div class="col-sm">
+                                <h1 style={{marginTop:"30px"}}>{user.nickname}</h1>
+                            </div>
+                            <div class="col-sm">
+                                <h4 style={{marginTop:"30px"}}>{user.userType}</h4>
+                            </div>
+                          </div>
+                      </li>
+                  )).reverse()}
+              </ul>
+            </div>
+            <div class="col-sm">
+              <h2 class="display-5"> Playlists: </h2>
+              <ul style={{padding:"0px", display:"inline"}}>
+                  {videosData.map(video => (
+                      <li className="search_list_item" class="mx-5" style={{marginTop:"20px", color:"white", borderRadius:"15px",
+                              padding:"20px", backgroundColor:"#222222", cursor: "pointer"}} onClick={() => handleVideoClick(video.id)}>
+                          <div class="row">
+                            <div className="box3" style={{width:"200px", height:"170px", cursor: "pointer"}}>
+                              <div className="box4" style={{width:"180px", height:"150px", backgroundImage: `url(${video.thumbnail})`, backgroundRepeat:"no-repeat", backgroundSize:"cover", backgroundPosition:"center"}}>
+                                  <table style={{backgroundColor: "transparent"}}>
+                                      <tr style={{backgroundColor: "transparent"}}>
+                                      <div className="movie_thumbnail" style={{width:"280px", height:"60px", backgroundSize:"cover", backgroundColor: "transparent" } } onClick={() => handleVideoClick(video.id)}>
 
-                                  </div>
-                                  </tr>
-                              </table>
-                            </div> 
-                        </div>
-                        <div class="col-sm">
-                            <h1 style={{marginTop:"30px"}}>{user.nickname}</h1>
-                        </div>
-                        <div class="col-sm">
-                            <h4 style={{marginTop:"30px"}}>{user.userType}</h4>
-                        </div>
-                      </div>
-                  </li>
-              )).reverse()}
-          </ul>
-            <h2 class="display-5"> Playlists: </h2>
-          <ul style={{padding:"0px", display:"inline"}}>
-              {videosData.map(video => (
-                  <li className="search_list_item" class="mx-5" style={{marginTop:"20px", color:"white", borderRadius:"15px",
-                          padding:"20px", backgroundColor:"#222222", cursor: "pointer"}} onClick={() => handleVideoClick(video.id)}>
-                      <div class="row">
-                        <div className="box3" style={{width:"300px", height:"170px", cursor: "pointer"}}>
-                          <div className="box4" style={{width:"280px", height:"150px", backgroundImage: `url(${video.thumbnail})`, backgroundRepeat:"no-repeat", backgroundSize:"cover", backgroundPosition:"center"}}>
-                              <table style={{backgroundColor: "transparent"}}>
-                                  <tr style={{backgroundColor: "transparent"}}>
-                                  <div className="movie_thumbnail" style={{width:"280px", height:"60px", backgroundSize:"cover", backgroundColor: "transparent" } } onClick={() => handleVideoClick(video.id)}>
-
-                                  </div>
-                                  </tr>
-                              </table>
-                            </div> 
-                        </div>
-                        <div class="col-sm">
-                            <h1 style={{marginTop:"30px"}}>{video.title}</h1>
-                            <h4 style={{marginTop:"30px"}}>{video.authorNickname}</h4>
-                        </div>
-                        <div class="col-sm">
-                            <h4 style={{marginTop:"30px"}}>Views: {video.viewCount}</h4>
-                        </div>
-                      </div>
-                  </li>
-              )).reverse()}
-          </ul>
-
+                                      </div>
+                                      </tr>
+                                  </table>
+                                </div> 
+                            </div>
+                            <div class="col-sm">
+                                <h1 style={{marginTop:"30px"}}>{video.title}</h1>
+                                <h4 style={{marginTop:"30px"}}>{video.authorNickname}</h4>
+                            </div>
+                            <div class="col-sm">
+                                <h4 style={{marginTop:"30px"}}>Views: {video.viewCount}</h4>
+                            </div>
+                          </div>
+                      </li>
+                  )).reverse()}
+              </ul>
+            </div>
+          </div>
     </div>
   );
 }

45ca85f0368e6105ec0a2f53d297e135ac587d63
wolny33 <patrykburzycki@op.pl>
2023-06-03 19:16:08 +0200

    loading spinner while uploading video added

--- Frontend/src/AddPlaylist.js
+++ Frontend/src/AddPlaylist.js
@@ -174,7 +174,7 @@ const AddPlaylist = () => {
                             <div>
                             <li style={{listStyleType: "none"}}>
                             <div className="box" style={{width:"300px", height:"170px"}}>
-                              <div className="box2" style={{width:"280px", height:"150px", backgroundImage: `url(${video.thumbnail})`, backgroundRepeat:"no-repeat", backgroundSize:"cover", backgroundPosition:"center"}}>
+                              <div className="box2" style={{width:"280px", height:"150px", backgroundImage: `url(${video.thumbnail})`, backgroundRepeat:"no-repeat", backgroundSize:"cover", backgroundPosition:"center", cursor: "pointer"}}>
                                   <table style={{backgroundColor: "transparent"}}>
                                       <tr style={{backgroundColor: "transparent"}}>
                                         <div className="movie_title" style={{width:"280px", height:"60px", fontSize:"10px", marginTop:"0", whiteSpace: 'nowrap', overflow: 'hidden', position:"center", color:"black", backgroundColor:"transparent" }}>
@@ -193,7 +193,7 @@ const AddPlaylist = () => {
                             <div>
                             <li style={{listStyleType: "none"}}>
                             <div className="box" style={{width:"300px", height:"170px", backgroundColor: "rgba(0, 255, 0, 0.3)"}}>
-                              <div className="box2" style={{width:"280px", height:"150px", backgroundImage: `url(${video.thumbnail})`, backgroundRepeat:"no-repeat", backgroundSize:"cover", backgroundPosition:"center"}}>
+                              <div className="box2" style={{width:"280px", height:"150px", backgroundImage: `url(${video.thumbnail})`, backgroundRepeat:"no-repeat", backgroundSize:"cover", backgroundPosition:"center", cursor: "pointer"}}>
                                   <table style={{backgroundColor: "rgba(0, 255, 0, 0.2)"}}>
                                       <tr style={{backgroundColor: "transparent"}}>
                                         <div className="movie_title" style={{width:"280px", height:"60px", fontSize:"10px", marginTop:"0", whiteSpace: 'nowrap', overflow: 'hidden', position:"center", color:"black", backgroundColor:"transparent" }}>

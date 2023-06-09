15e8994c1b30b9500f4ca8c3ee3144c62cebc38c
Maciej Podgórski <01151542@pw.edu.pl>
2023-06-09 23:57:02 +0200

    poprawka

--- MyWideIO.API/Services/VideoService.cs
+++ MyWideIO.API/Services/VideoService.cs
@@ -72,11 +72,6 @@ namespace MyWideIO.API.Services
                 case ProcessingProgressEnum.Processing:
                     throw new VideoException("can't delete while processing");
             }
-            // TODO: usuwanie przez admina, blokada usuwania jak jest nie resolved ticket
-           // if (await _ticketRepository.TargetHasTickets(videoId))
-            //    throw new TicketException("Can't remove video while there are tickets for it");
-            var tickets = await _ticketRepository.GetTargetsTickets(videoId);
-            await _ticketRepository.RemoveAsync(tickets);
             // delete likes
             var likes = await _likeRepository.GetVideoLikesAsync(videoId);
             await _likeRepository.RemoveAsync(likes);
